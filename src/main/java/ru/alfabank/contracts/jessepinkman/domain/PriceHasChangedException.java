703f95da57a4b9649c7efef22a078ef2f4b695fb
Artem Ptushkin <aptushkin@alfabank.ru>
2019-07-26 13:55:14 +0300

    added exception reason

--- src/main/java/ru/alfabank/contracts/jessepinkman/domain/PriceHasChangedException.java
+++ src/main/java/ru/alfabank/contracts/jessepinkman/domain/PriceHasChangedException.java
@@ -3,7 +3,7 @@ package ru.alfabank.contracts.jessepinkman.domain;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Price has changed, b$tch")
 public class PriceHasChangedException extends RuntimeException {
 	public PriceHasChangedException(String message) {
 		super(message);
